d89f63088397fa6ee8259f15e403b146dcff22e9
Mykhailo Balakhon <9mohapx9@gmail.com>
2025-03-24 09:55:55 +0100

    Init openapi.yaml

---
+++ src/main/java/ua/mibal/Application.java
@@ -0,0 +1,12 @@
+package ua.mibal;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+public class Application {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
