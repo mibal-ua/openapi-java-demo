c8896acce9ba75c00b57627f947e7bbb34fb3ded
Mykhailo Balakhon <9mohapx9@gmail.com>
2025-04-01 12:14:41 +0200

    Setup `Application`

--- src/main/java/ua/mibal/Application.java
+++ src/main/java/ua/mibal/Application.java
@@ -1,12 +1,16 @@
 package ua.mibal;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@SpringBootApplication
 public class Application {
 
-	public static void main(final String[] args) {
-		System.out.println("Hello, World!");
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 }
